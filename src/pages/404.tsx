16eb1db612091686e7ac42e8327871b4e320d344
cuteolaf <oliverlim818@gmail.com>
2023-06-10 12:09:56 -0700

    error page

--- src/pages/404.tsx
+++
@@ -1,5 +0,0 @@
-const Page404 = () => {
-  return <div>This page could not be found</div>;
-};
-
-export default Page404;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
 import { CacheProvider, EmotionCache } from '@emotion/react';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
+import { NextPage } from 'next';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import * as React from 'react';
+import { UseInkProvider } from 'useink';
+import { RococoContractsTestnet, ShibuyaTestnet } from 'useink/chains';
 import '../../styles/global.scss';
 
 import createEmotionCache from '@/utils/createEmotionCache';
@@ -13,13 +16,18 @@ import { Layout } from '@/components/Layout';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
-
+export type NextPageWithLayout<P = unknown, IP = P> = NextPage<P, IP> & {
+  getLayout?: (_page: React.ReactElement) => React.ReactNode;
+};
 interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
+  Component: NextPageWithLayout;
 }
 
 export default function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const getLayout = Component.getLayout ?? ((page) => <Layout>{page}</Layout>);
+
   return (
     <CacheProvider value={emotionCache}>
       <Head>
@@ -27,11 +35,16 @@ export default function MyApp(props: MyAppProps) {
         <title>Dotflow UI</title>
       </Head>
       <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <UseInkProvider
+          config={{
+            dappName: 'Dotflow UI',
+            chains: [RococoContractsTestnet, ShibuyaTestnet],
+          }}
+        >
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          {getLayout(<Component {...pageProps} />)}
+        </UseInkProvider>
       </ThemeProvider>
     </CacheProvider>
   );
