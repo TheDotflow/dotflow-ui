8aeabfe40e48de1e83feb298b97229fc55ecb7cc
Sergej Sakac <sakacszergej@gmail.com>
2023-06-15 16:31:42 +0200

    Fix warning

--- src/contracts/identity/context.tsx
+++ src/contracts/identity/context.tsx
@@ -39,7 +39,7 @@ interface Props {
 }
 
 const IdentityContractProvider = ({ children }: Props) => {
-  const { contract } = useContract(IdentityMetadata);
+  const { contract } = useContract(IdentityMetadata, CONTRACT_IDENTITY);
   const { api, activeAccount } = useInkathon();
   const [identityNo, setIdentityNo] = useState<number | null>(null);
 
