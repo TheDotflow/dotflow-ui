0a52c61f92d41339ae0b6aadc9a22464c98aed36
Sergej Sakac <73715684+Szegoo@users.noreply.github.com>
2023-07-03 16:38:31 +0200

    Asset registry (#40)
    * asset registry; not tested
    
    * test
    
    * don't hardcode the network
    
    * remove console
    
    * refactor
    
    * fix: address book page not working
    
    ---------
    
    Co-authored-by: cuteolaf <oliverlim818@gmail.com>

---
+++ src/utils/assetRegistry.ts
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+type Asset = {
+  asset: any,
+  name: string,
+  symbol: string,
+  decimals: number,
+  xcmInteriorKey?: any,
+  inferred: boolean,
+  confidence: number
+};
+
+const xcmGAR = "https://cdn.jsdelivr.net/gh/colorfulnotion/xcm-global-registry/metadata/xcmgar_url.json";
+
+class AssetRegistry {
+  public static async getAssetsOnBlockchain(network: "polkadot" | "kusama", chain: string): Promise<Asset[]> {
+    const blockchains = (await axios.get(xcmGAR)).data;
+
+    const blockchain = blockchains.assets[network].find((b: any) => b.id.toLowerCase() == chain.toLowerCase());
+
+    if (!blockchain) {
+      throw new Error("Blockchain not found");
+    }
+
+    const assetsUrl = blockchain.url;
+
+    const assets: Asset[] = (await axios.get(assetsUrl)).data;
+
+    assets.map((asset) => {
+      if (asset.xcmInteriorKey) {
+        asset.xcmInteriorKey = JSON.parse(asset.xcmInteriorKey);
+      }
+    });
+
+    return assets;
+  }
+}
+
+export default AssetRegistry;
