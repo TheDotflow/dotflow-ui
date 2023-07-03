7e5cb9c54fc15f17cc1c4873a04d215f7c0ff520
cuteolaf <oliverlim818@gmail.com>
2023-07-03 07:30:58 -0700

    refactor

--- src/utils/assetRegistry.ts
+++ src/utils/assetRegistry.ts
@@ -16,23 +16,18 @@ class AssetRegistry {
   public static async getAssetsOnBlockchain(network: "polkadot" | "kusama", chain: string): Promise<Asset[]> {
     const blockchains = (await axios.get(xcmGAR)).data;
 
-    let blockchain;
+    const blockchain = blockchains.assets[network].find((b: any) => b.id.toLowerCase() == chain.toLowerCase());
 
-    if(network == "polkadot") {
-      blockchain = blockchains.assets.polkadot.find((b:any) => b.id.toLowerCase() == chain.toLowerCase());
-    }else if(network == "kusama") {
-      blockchain = blockchains.assets.kusama.find((b:any) => b.id.toLowerCase() == chain.toLowerCase());
-    }
-
-    if(!blockchain) {
+    if (!blockchain) {
       throw new Error("Blockchain not found");
     }
 
     const assetsUrl = blockchain.url;
 
     const assets: Asset[] = (await axios.get(assetsUrl)).data;
+
     assets.map((asset) => {
-      if(asset.xcmInteriorKey) {
+      if (asset.xcmInteriorKey) {
         asset.xcmInteriorKey = JSON.parse(asset.xcmInteriorKey);
       }
     });
