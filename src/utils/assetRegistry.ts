f622defdadf7ca15c4d1ff3f8e0ff741206d1916
cuteolaf <oliverlim818@gmail.com>
2023-07-10 15:38:31 -0700

    add type: MultiAsset, fmt

--- src/utils/assetRegistry.ts
+++ src/utils/assetRegistry.ts
@@ -1,25 +1,40 @@
 import axios from 'axios';
 
 type Asset = {
-  asset: any,
-  name: string,
-  symbol: string,
-  decimals: number,
-  xcmInteriorKey?: any,
-  inferred: boolean,
-  confidence: number
+  asset: any;
+  name: string;
+  symbol: string;
+  decimals: number;
+  xcmInteriorKey?: any;
+  inferred: boolean;
+  confidence: number;
 };
 
-const xcmGAR = "https://cdn.jsdelivr.net/gh/colorfulnotion/xcm-global-registry/metadata/xcmgar_url.json";
+type MultiAsset = {
+  parents: number;
+  interior:
+    | 'Here'
+    | {
+        [key: string]: Asset[];
+      };
+};
+
+const xcmGAR =
+  'https://cdn.jsdelivr.net/gh/colorfulnotion/xcm-global-registry/metadata/xcmgar_url.json';
 
 class AssetRegistry {
-  public static async getAssetsOnBlockchain(network: "polkadot" | "kusama", chain: string): Promise<Asset[]> {
+  public static async getAssetsOnBlockchain(
+    network: 'polkadot' | 'kusama',
+    chain: string
+  ): Promise<Asset[]> {
     const blockchains = (await axios.get(xcmGAR)).data;
 
-    const blockchain = blockchains.assets[network].find((b: any) => b.id.toLowerCase() == chain.toLowerCase());
+    const blockchain = blockchains.assets[network].find(
+      (b: any) => b.id.toLowerCase() == chain.toLowerCase()
+    );
 
     if (!blockchain) {
-      throw new Error("Blockchain not found");
+      throw new Error('Blockchain not found');
     }
 
     const assetsUrl = blockchain.url;
@@ -35,17 +50,23 @@ class AssetRegistry {
     return assets;
   }
 
-  public static xcmInteriorToMultiAsset(xcmInteriorKey: any[], isParachain: boolean) {
+  public static xcmInteriorToMultiAsset(
+    xcmInteriorKey: any[],
+    isParachain: boolean
+  ): MultiAsset {
     // The first 'junction' is actually just the specifying the network and we
     // don't need that in `MultiAsset`.
     const junctionCount = xcmInteriorKey.length - 1;
-    const parents = isParachain? 1 : 0;
+    const parents = isParachain ? 1 : 0;
 
-    if(junctionCount == 1 && xcmInteriorKey[1].toString().toLowerCase() == "here") {
+    if (
+      junctionCount == 1 &&
+      xcmInteriorKey[1].toString().toLowerCase() == 'here'
+    ) {
       return {
         parents,
-        interior: "Here" 
-      }
+        interior: 'Here',
+      };
     }
 
     const junctions = this.getJunctions(xcmInteriorKey, junctionCount);
@@ -54,19 +75,17 @@ class AssetRegistry {
     return {
       parents,
       interior: {
-        [x]: junctions
-      }
-    }
+        [x]: junctions,
+      },
+    };
   }
 
-  private static getJunctions(xcmInteriorKey: any[], junctionCount: number): any[] {
-    const junctions = [];
-    for(let i = 1; i <= junctionCount; i++) {
-      junctions.push(xcmInteriorKey[i]);
-    }
-
-    return junctions;
-  } 
+  private static getJunctions(
+    xcmInteriorKey: any[],
+    junctionCount: number
+  ): any[] {
+    return xcmInteriorKey.slice(1, junctionCount + 1);
+  }
 }
 
 export default AssetRegistry;
